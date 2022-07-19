e311237c11b639168f25e5931c758c18843c9910
GoldenGuy1000 <73797581+GoldenGuy1000@users.noreply.github.com>
2022-07-19 16:09:22 -0700

    refactored patches to be in a separate class

---
+++ HarmonyPatches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace NormaliseNoteRumble
+{
+    [HarmonyPatch]
+    public class HarmonyPatches
+    {
+        [HarmonyPatch(typeof(SliderHapticFeedbackInteractionEffect), nameof(SliderHapticFeedbackInteractionEffect.Update))]
+        [HarmonyPrefix]
+        public static bool SliderHapticPatch()
+        {
+            bool noRumble = true;
+            // Plugin.Log.Info("the SliderHapticFeedbackInteractionEffect class is existing");
+            if (noRumble)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -39,21 +39,4 @@ namespace NormaliseNoteRumble
         }
 
     }
-
-    [HarmonyPatch]
-    public class HarmonyPatches
-    {
-        [HarmonyPatch(typeof(SliderHapticFeedbackInteractionEffect), nameof(SliderHapticFeedbackInteractionEffect.Update))]
-        [HarmonyPrefix]
-        public static bool SliderHapticPatch()
-        {
-            bool noRumble = true;
-            // Plugin.Log.Info("the SliderHapticFeedbackInteractionEffect class is existing");
-            if (noRumble)
-            {
-                return false;
-            }
-            return true;
-        }
-    }
 }
