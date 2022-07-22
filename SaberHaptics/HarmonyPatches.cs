ce6c90c671e5abe995886afe782d34bc71892b2c
GoldenGuy1000 <73797581+GoldenGuy1000@users.noreply.github.com>
2022-07-21 23:35:56 -0700

    I spent all day writing a transpiler & I very much does not need a transpiler it will be removed in the next commit

--- SaberHaptics/HarmonyPatches.cs
+++ SaberHaptics/HarmonyPatches.cs
@@ -1,4 +1,8 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace SaberHaptics
 {
@@ -12,9 +16,44 @@ namespace SaberHaptics
             return Configuration.Instance.ArcRumble; // (run the method if arcrumble is enabled, don't if it isn't)
         }
 
-        [HarmonyPatch(typeof(NoteCutCoreEffectsSpawner), nameof(NoteCutCoreEffectsSpawner.HandleNoteWasCut))]
-        [HarmonyPrefix]
-        static bool HapticIntensityPatch(NoteController noteController, in NoteCutInfo noteCutInfo,
+		[HarmonyPatch(typeof(NoteCutCoreEffectsSpawner), nameof(NoteCutCoreEffectsSpawner.HandleNoteWasCut))]
+		[HarmonyTranspiler]
+		static IEnumerable<CodeInstruction> HapticIntensityPatch(IEnumerable<CodeInstruction> instructions) // those for loops neet to get refactored or something sheesh
+        {
+			var codes = new List<CodeInstruction>(instructions);
+
+			List<int> switchBranchStarts = new List<int>();
+
+			for (int i = 0; i < codes.Count; i++)
+            {
+				if (codes[i].opcode == OpCodes.Switch)
+				{
+					//  Plugin.Log.Info("found the switch @ " + i);
+					Label[] switchCases = ((Label[])codes[i].operand);
+					for (int noteType = 0; noteType < switchCases.Length; noteType++) // loop through the arms
+                    {
+						for (int instructionIndex = i; instructionIndex < codes.Count; instructionIndex++) // loop through instructions until you find the arms
+                        {
+							if (codes[instructionIndex].labels.Contains(switchCases[noteType])) {
+								//  Plugin.Log.Info("start of " + noteType + ": " + instructionIndex);
+								switchBranchStarts.Append(instructionIndex);
+                            }
+                        }
+						// switchCases[noteType]
+                    }
+				}
+
+				if (codes[i].Calls(typeof(NoteCutHapticEffect).GetMethod("HitNote")))
+                {
+					Plugin.Log.Info("hittin the note @ " + i);
+                }
+			}
+
+			return codes.AsEnumerable();
+        }
+
+		[System.Obsolete("HapticIntensityPatch is a transpiler for what this was supposed to do")]
+        static bool OldHapticIntensityPrefix(NoteController noteController, in NoteCutInfo noteCutInfo,
 			NoteCutCoreEffectsSpawner __instance, NoteCutHapticEffect ____noteCutHapticEffect, AudioTimeSyncController ____audioTimeSyncController)
         {
 			if (noteController.noteData.time + 0.5f < ____audioTimeSyncController.songTime)
