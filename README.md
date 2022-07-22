b312b9ac93d42281af1cb43f9b7092caa437e3f8
GoldenGuy1000 <73797581+GoldenGuy1000@users.noreply.github.com>
2022-07-22 13:45:02 -0700

    slicin does the exact same thing as the ordinary game (hitting notes gives the normal feedback as it should) I also added the unity xr module since I need it for haptic feedback

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # SaberHaptics
 A Beat Saber mod to change the haptic feedback for your sabers (hitting notes, sticking your sabers in walls, etc.)
 
+^ menu haptics are unaffected
+
 
 ## Settings
 Arc Rumble: toggles controller rumble from arc notes
@@ -9,14 +11,17 @@ Arc Rumble: toggles controller rumble from arc notes
 ## Harmony Patches
 (don't worry about this unless you're also making a mod that also interacts with these methods)
 
-### prefixes
+### Prefixes
 `SliderHapticFeedbackInteractionEffect.Update` <- skips update if ArcRumble is disabled  
-`NoteCutCoreEffectsSpawner.HandleNoteWasCut` <- completely replaces this method [1]
+`NoteCutCoreEffectsSpawner.HandleNoteWasCut` <- will not skip (just gets the note type)
+`NoteCutHapticEffect.HitNote` <- replaced
 
 
 
 # Build Instructions
+- Install beatsaber, bsipa, & bsml
 - Make SaberHaptics.csproj.user & put beat saber directory in there
+- build with VS
 
 ```xml
 <Project>
@@ -28,5 +33,3 @@ Arc Rumble: toggles controller rumble from arc notes
 	</PropertyGroup>
 </Project>
 ```
-
-[1] (other implementations like changing the amount of rumble based on what the preset corrosponded to felt jank)
--- SaberHaptics/HarmonyPatches.cs
+++ SaberHaptics/HarmonyPatches.cs
@@ -1,14 +1,13 @@
 ﻿using HarmonyLib;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
+using Libraries.HM.HMLib.VR;
 
 namespace SaberHaptics
 {
     [HarmonyPatch]
     class HarmonyPatches
     {
+		static NoteData.GameplayType lastNoteCut;
+
         [HarmonyPatch(typeof(SliderHapticFeedbackInteractionEffect), nameof(SliderHapticFeedbackInteractionEffect.Update))]
         [HarmonyPrefix]
         static bool SliderHapticPatch()
@@ -17,40 +16,46 @@ namespace SaberHaptics
         }
 
 		[HarmonyPatch(typeof(NoteCutCoreEffectsSpawner), nameof(NoteCutCoreEffectsSpawner.HandleNoteWasCut))]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> HapticIntensityPatch(IEnumerable<CodeInstruction> instructions) // those for loops neet to get refactored or something sheesh
+		[HarmonyPrefix]
+		static void StoreNoteType(NoteController noteController)
         {
-			var codes = new List<CodeInstruction>(instructions);
+			lastNoteCut = noteController.noteData.gameplayType;
+			Plugin.Log.Info("Note Type: " + lastNoteCut);
+        }
 
-			List<int> switchBranchStarts = new List<int>();
-
-			for (int i = 0; i < codes.Count; i++)
+		[HarmonyPatch(typeof(NoteCutHapticEffect), nameof(NoteCutHapticEffect.HitNote))]
+		[HarmonyPrefix]
+		static bool NoteHapticsPatch(SaberType saberType,
+			HapticPresetSO ____normalPreset, HapticPresetSO ____shortNormalPreset, HapticPresetSO ____shortWeakPreset,
+			HapticFeedbackController ____hapticFeedbackController)
+        {
+			HapticPresetSO hapticPreset;
+			switch (lastNoteCut)
             {
-				if (codes[i].opcode == OpCodes.Switch)
-				{
-					//  Plugin.Log.Info("found the switch @ " + i);
-					Label[] switchCases = ((Label[])codes[i].operand);
-					for (int noteType = 0; noteType < switchCases.Length; noteType++) // loop through the arms
-                    {
-						for (int instructionIndex = i; instructionIndex < codes.Count; instructionIndex++) // loop through instructions until you find the arms
-                        {
-							if (codes[instructionIndex].labels.Contains(switchCases[noteType])) {
-								//  Plugin.Log.Info("start of " + noteType + ": " + instructionIndex);
-								switchBranchStarts.Append(instructionIndex);
-                            }
-                        }
-						// switchCases[noteType]
-                    }
-				}
-
-				if (codes[i].Calls(typeof(NoteCutHapticEffect).GetMethod("HitNote")))
-                {
-					Plugin.Log.Info("hittin the note @ " + i);
-                }
+				case NoteData.GameplayType.Normal:
+					hapticPreset = ____normalPreset;
+					break;
+				case NoteData.GameplayType.Bomb:
+					hapticPreset = ____normalPreset;
+					break;
+				case NoteData.GameplayType.BurstSliderHead:
+					hapticPreset = ____shortNormalPreset;
+					break;
+				case NoteData.GameplayType.BurstSliderElement:
+					hapticPreset = ____shortWeakPreset;
+					break;
+				default:
+					hapticPreset = ____normalPreset;
+					break;
 			}
 
-			return codes.AsEnumerable();
+			____hapticFeedbackController.PlayHapticFeedback(saberType.Node(), hapticPreset);
+
+			return false;
         }
+
+
+
 
 		[System.Obsolete("HapticIntensityPatch is a transpiler for what this was supposed to do")]
         static bool OldHapticIntensityPrefix(NoteController noteController, in NoteCutInfo noteCutInfo,
